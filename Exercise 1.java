aad22812edc264d2440b7bdb59af8b15987522f4
pramdevgan <61196674+pramdevgan@users.noreply.github.com>
2021-07-18 01:01:17 +0530

    Update Exercise 1.java

--- Exercise 1.java
+++ Exercise 1.java
@@ -1,3 +1,15 @@
+// Conditionals Exercise 1
+// Problem
+// Use the variable x as you write this program. x will represent a positive integer. Write a program that determines if x is between 0 and 25 or between 75 and 100. If yes, print the message:_ is between 0 and 25 or 75 and 100, where the blank would be the value of x. The program should do nothing if the value of x does not fit into either range.
+// Note: These are inclusive ranges - 0, 25, 75 and 100 should print out the message.
+// Type Casting
+
+// Expected Output
+// If x is 8, then the output would be: 8 is between 0 and 25 or 75 and 100.
+// If x is 80, then the output would be: 80 is between 0 and 25 or 75 and 100.
+// If x is 100, then the output would be: 100 is between 0 and 25 or 75 and 100.
+
+              <h1> Solution<h1>
 public class Exercise1 {
   public static void main(String args[]) {
     
