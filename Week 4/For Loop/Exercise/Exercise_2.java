b7fb0dd735c7682c4873c35b3bb57df7beda3e73
pramdevgan <61196674+pramdevgan@users.noreply.github.com>
2021-07-21 02:14:25 +0530

    Create Exercise_2.java

---
+++ Week 4/For Loop/Exercise/Exercise_2.java
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                                                                        // Print Ten Times
+
+// Write a program that takes in a string x from the user as an argument and then prints that information ten times using a loop.
+
+
+// Requirements
+// You must include a for or while loop in your code.
+// You must include one and only one System.out.println statement in your code.
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                                          // Solution 1
+
+for (int i = 0; i < 10; i++) {
+  System.out.println(x);
+}
+
+
+                                          // Solution 2
+
+
+int i = 0;
+while (i < 10) {
+  System.out.println(x);
+  i++;
+}
